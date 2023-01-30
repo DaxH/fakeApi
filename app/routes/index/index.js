204d397d43767026609ac3e240a7a0bd915864c0
jr-98 <jrtillaguango@gmail.com>
2023-01-30 09:11:09 -0500

    Se agrego datos nuevcs referentes a movimientos

--- app/routes/index/index.js
+++ app/routes/index/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const transfer = require('../transfer/index')
 const otp = require('../otp/index')
+const movements = require('../movements/index')
 
 router.use('/transfer', transfer)
 router.use('/otp', otp)
-
+router.use('/movements', movements)
 router.get('/', function (req, res) {
 	res.status(200).json({ message: 'Estás conectado a nuestra API' })
 })
