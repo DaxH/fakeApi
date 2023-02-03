69855081f63573f608fb106e927a4d5b18146e2a
DaxH <52769764+DaxH@users.noreply.github.com>
2023-02-03 12:04:10 -0500

    pago de servicios

--- app/routes/index/index.js
+++ app/routes/index/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 const transfer = require('../transfer/index')
 const otp = require('../otp/index')
+const service = require('../servicePay/index')
 
 router.use('/transfer', transfer)
 router.use('/otp', otp)
+router.use('/services', service)
 
 router.get('/', function (req, res) {
 	res.status(200).json({ message: 'Estás conectado a nuestra API' })
